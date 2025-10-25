8eb886b937ffa8abdc35654b2a384ebc83406199
Ugoyoungking <ugochukwujonathan067@gmail.com>
2025-10-25 03:24:43 +0100

    Update next-seo.config.js

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,26 +1,26 @@
 export default {
-  title: 'Ugochukwu Jonathan | Ugoyoungking',
-  description: 'Web Developer & Graphic Designer based in Nigeria. Creating modern, responsive web experiences.',
+  title: "Ugochukwu Jonathan | Ugoyoungking",
+  description:
+    "Web Developer & Graphic Designer building fast, modern, and creative web experiences using Next.js and Firebase.",
   openGraph: {
-    type: 'profile',
-    locale: 'en_US',
-    url: 'https://pro-portfolio-ten.vercel.app/',
-    title: 'Ugochukwu Jonathan | Ugoyoungking',
-    description: 'Web Developer & Graphic Designer based in Nigeria.',
+    type: "profile",
+    locale: "en_US",
+    url: "https://pro-portfolio-ten.vercel.app/",
+    title: "Ugochukwu Jonathan | Ugoyoungking",
+    description: "Web Developer & Graphic Designer based in Nigeria.",
     images: [
       {
-        url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Ugochukwu_Jonathan_%28Ugoyoungking%29.jpg/336px-Ugochukwu_Jonathan_%28Ugoyoungking%29.jpg?20251023225125',
+        url: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Ugochukwu_Jonathan_%28Ugoyoungking%29.jpg/336px-Ugochukwu_Jonathan_%28Ugoyoungking%29.jpg?20251023225125",
         width: 336,
         height: 336,
-        alt: 'Ugochukwu Jonathan (Ugoyoungking)',
+        alt: "Ugochukwu Jonathan (Ugoyoungking)",
       },
     ],
-    site_name: 'Ugochukwu Jonathan Portfolio',
+    site_name: "Ugochukwu Jonathan Portfolio",
   },
   twitter: {
-    handle: '@ugoyoungking',
-    site: '@ugoyoungking',
-    cardType: 'summary_large_image',
+    handle: "@ugoyoungking",
+    site: "@ugoyoungking",
+    cardType: "summary_large_image",
   },
 };
-
